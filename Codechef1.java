57d4d8e0adabe01df72eed819438fb31267c55b5
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-06 11:26:07 +0530

    Add files via upload

---
+++ Codechef1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int n = sc.nextInt();
+		if(n<=60)
+		
+		System.out.println("yes ");
+		
+		else
+		System.out.println("no");
+		
+	
+
+	}
+}
