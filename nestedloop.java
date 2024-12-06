0a54b15b7c20ef5df8c50a94ada2e04c01a3fc4e
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-06 12:24:10 +0530

    Add files via upload

---
+++ nestedloop.java
@@ -0,0 +1,17 @@
+public class nestedloop {
+
+    public static void main(String args[])
+    
+    {
+        int n = 5;
+        for(int i=1;i<=n;i++ ){
+            for(int j = 1;j<=5;j++){
+                System.out.print((char)(j+64)+" ");
+            }
+
+            System.out.println();
+        }
+
+    }
+    
+}
